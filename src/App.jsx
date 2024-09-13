b5850ca889cc985916b21e2355ce5954346448e9
unknown <hiruthickbs@gmail.com>
2024-09-13 13:12:59 +0530

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import SideBar from "./Components/SideBar";
 
 function App() {
   const[data,setData] = useState(null);
-  const[loading,setLoading] = useState(false);
+  const[loading,setLoading] = useState(true);
   
   const[showModal,setShowModal] = useState(false);
 
@@ -17,19 +17,22 @@ function App() {
 
     async function fetchAPIData() {
       const NASA_KEY = import.meta.env.VITE_NASA_API_KEY; // This variable has the key to the Api
-      const url ='https://api.nasa.gov/planetary/apod'+`?api_key=${NASA_KEY}`
-
-      const today = (new Date()).toDateString();
+      const today = new Date().toISOString().split('T')[0];
       const localKey = `NASA-${today}`
-      if(localStorage.getItem(localKey)){
-        const apiData = JSON.parse(localStorage.getItem(localKey));
-        setData(apiData);
+
+      const cachedData =localStorage.getItem(localKey);
+      if(cachedData){
+        setData(JSON.parse(cachedData));
         console.log("Fetched from Cache today")
-        return;
       } 
-      localStorage.clear()
+      else{
+         const url ='https://api.nasa.gov/planetary/apod'+`?api_key=${NASA_KEY}`
+      // localStorage.clear()
       try{
-        const res = await fetch(url);
+        const res = await fetch(url,{cache:"no-store"});
+        if(!res.ok){
+          throw new Error("Failed to fetch APOD API");
+        }
         const apiData = await res.json();
         localStorage.setItem(localKey,JSON.stringify(apiData));
         setData(apiData);
@@ -39,6 +42,8 @@ function App() {
         console.log(err.message)
       }
     }
+    setLoading("false");
+  }
     fetchAPIData(); 
 
   },[])
